412bae3559576459d9c73774d26ae5b12c4cbbdf
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2025-12-10 13:56:24 +0100

    test: python tests

--- stations/test_station1.py
+++ stations/test_station1.py
@@ -12,7 +12,7 @@ def is_valid_humidity(h):
 
 
 def test_temperature_valid_range():
-    t = 30
+    t = -999
     assert is_valid_temperature(t)
 
 
@@ -22,7 +22,7 @@ def test_temperature_failure_case():
 
 
 def test_humidity_range():
-    h = 45.5
+    h = 120
     assert is_valid_humidity(h)
 
 def test_humidity_failure_case():
