4ef4d1757ac4c9b7b070469a401b558da7f5e034
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2025-12-16 10:08:10 +0100

    feat: added validation tests

--- stations/test_station1.py
+++ stations/test_station1.py
@@ -1,31 +1,25 @@
 import re
 import time
 import pytest
-
-
-def is_valid_temperature(t):
-    return isinstance(t, (int, float)) and t >= -273
-
-def is_valid_humidity(h):
-    return isinstance(h, (int, float)) and 0 <= h <= 100
+import station1
 
 
 
 def test_temperature_valid_range():
     t = 30
-    assert is_valid_temperature(t)
+    assert station1.validate_temperature(t)
 
 
 def test_temperature_failure_case():
     t = -999
-    assert not is_valid_temperature(t)
+    assert not station1.validate_temperature(t)
 
 
 def test_humidity_range():
-    h = 45.5
-    assert is_valid_humidity(h)
+    h = 45
+    assert station1.validate_humidity(h)
 
 def test_humidity_failure_case():
     h = 120
-    assert not is_valid_humidity(h)
+    assert not station1.validate_temperature(h)
 
