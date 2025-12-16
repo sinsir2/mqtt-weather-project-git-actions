f69e11373332b92e721f4e0eca340baf02799afc
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2025-12-16 10:35:27 +0100

    chore: changed tabs to spaces

--- stations/test_station1.py
+++ stations/test_station1.py
@@ -4,17 +4,17 @@ import pytest
 import station1
 
 def test_temperature_valid_range():
-	t = 30
-	assert station1.validate_temperature(t)
+    t = 30
+    assert station1.validate_temperature(t)
 
 def test_temperature_failure_case():
-	t = -999
-	assert not station1.validate_temperature(t)
+    t = -999
+    assert not station1.validate_temperature(t)
 
 def test_humidity_range():
-	h = 45
-	assert station1.validate_humidity(h)
+    h = 45
+    assert station1.validate_humidity(h)
 
 def test_humidity_failure_case():
-	h = 120
-	assert not station1.validate_temperature(h)
+    h = 120
+    assert not station1.validate_temperature(h)
